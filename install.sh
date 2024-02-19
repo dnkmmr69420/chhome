5d8bd2327d0d98e12eb60fcccf310a1d7b01e163
dnkmmr <104856296+dnkmmr69420@users.noreply.github.com>
2024-02-19 14:34:28 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 if command -v <go> &> /dev/null
 then
-    go build -o $PREFIX/chhome main.go
+    go build -o $1/chhome main.go
 else
     echo "Go is not detected. Make sure Go is in your PATH"
     exit 1
